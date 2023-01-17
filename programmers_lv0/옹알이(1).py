db427622948e8a46dd1ea1476ca6c63893d35c34
potatowon <118495946+potatowon@users.noreply.github.com>
2023-01-18 01:51:55 +0900

    Update 옹알이(1).py

--- programmers_lv0/옹알이(1).py
+++ programmers_lv0/옹알이(1).py
@@ -4,11 +4,8 @@ cnt = 0
 for b in babbling:
     for i in word:
         b = b.replace(i, ',')
-    print(b)
-    print(len(b))
     if (b == ',') or(b == ',,') or (b ==',,,') or(b == ',,,,'):
         cnt += 1
-print(cnt)
 a = babbling[0].replace(word[3], '')
 
 
