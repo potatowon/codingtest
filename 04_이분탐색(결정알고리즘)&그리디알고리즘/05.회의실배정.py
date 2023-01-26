de57205fcb65caa5ea04df48c45e1d5763284784
potatowon <118495946+potatowon@users.noreply.github.com>
2023-01-26 23:45:54 +0900

    0126

--- 04_이분탐색(결정알고리즘)&그리디알고리즘/05.회의실배정.py
+++ 04_이분탐색(결정알고리즘)&그리디알고리즘/05.회의실배정.py
@@ -2,16 +2,23 @@
 n 개의 회의들에 대해아ㅕ 회의실 사용표를 만들려고 한다.
 시작시간 : 끝나는 시간
 겹치지 않게 회의실을 사용할 수 있는 최대수의 회의를 찾아라
-
-
 '''
+
+# 아이디어 : 끝나는 시간을 기준으로 정렬하라
+# 빨리끝나는것이 중요함.. 그래야 회의 갯수를 많이 할 수 있다
 
 import sys
 
-n = int(input())
-
-tl = [list(map(int, sys.stdin.readline().split())) for _ in range(n)] # time list
-tl.sort(key=lambda x: abs(x[0]-x[1]))
-# tl.sort(key=lambda x: x[0])
-
-print(tl)
+n=int(input())
+meeting=[]
+for i in range(n):
+    a, b=map(int, input().split())
+    meeting.append((a, b))
+meeting.sort(key=lambda x : (x[1], x[0])) ## 정렬 순위는 튜플로 정해주면 된다.
+et=0
+cnt=0
+for x, y in meeting:
+    if x>=et:
+        et=y
+        cnt+=1
+print(cnt)
