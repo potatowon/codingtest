752a13960f8d95202d4bf0f408cfec09900288ac
potatowon <yewkim0203@gmail.com>
2023-02-20 10:51:08 +0900

    0220

--- 코테알고리즘_백준/연속된 자연수의 합 구하기.py
+++ 코테알고리즘_백준/연속된 자연수의 합 구하기.py
@@ -16,23 +16,48 @@ import sys
 
 n = int(sys.stdin.readline())
 
-numbers = [i for i in range(16)]
 
 start_idx, end_idx = 1, 1
 cnt = 1
+sum_ = 1
 
 
 while end_idx != n:
-    sum_ = sum(numbers[start_idx:end_idx+1])
+
     if sum_ == n:
        cnt += 1
        end_idx += 1
+       sum_ += end_idx
     elif sum_ < n:
         end_idx += 1
+        sum_ += end_idx
     else:
-        start_idx += 1 
+        sum_ -= start_idx
+        start_idx += 1
 
 print(cnt)
 
 
+'''
+프로그래머스 - 숫자의 표현 (비슷한 문제) ; 프로그래머스는 정답이지만, 백준은 메모리 초과
 
+def solution(n):
+    numbers = [i for i in range(n+1)]
+
+    start_idx, end_idx = 1, 1
+    cnt = 1
+
+
+    while end_idx != n:
+        sum_ = sum(numbers[start_idx:end_idx+1])
+
+        if sum_ == n:
+            cnt += 1
+            end_idx += 1
+        elif sum_ < n:
+            end_idx += 1
+        else:
+            start_idx += 1
+    return cnt
+
+'''
