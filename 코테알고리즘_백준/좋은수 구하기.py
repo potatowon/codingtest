c8b5a27c119d607c6824f4a917f184fc20d1b87b
potatowon <yewkim0203@gmail.com>
2023-02-22 13:15:50 +0900

    좋은수 구하기 리뷰

--- 코테알고리즘_백준/좋은수 구하기.py
+++ 코테알고리즘_백준/좋은수 구하기.py
@@ -11,17 +11,17 @@ num.sort()
 cnt = 0
 
 
-for k in range(n):
-    tmp = num[:k] + num[k+1:] # 자기 자신의 인덱스를 제외함.
-    i, j = 0, len(tmp)-1
+for k in range(n):              # 각 num의 모든 원소를 돌아야 하므로, 인덱스 설정
+    tmp = num[:k] + num[k+1:]   # 자기 자신의 인덱스를 제외함.
+    i, j = 0, len(tmp)-1        # 포인터를 양끝에서 부터 시작 -> 초기화
     while i < j:
-        t = tmp[i] + tmp[j]
-        if t == num[k]:
-            cnt += 1
+        t = tmp[i] + tmp[j]     # 포인터 두 수의 합
+        if t == num[k]:         
+            cnt += 1            # 좋은 수를 만족하는 경우 수 증가 후 break
             break
-        elif t < num[k]:
+        elif t < num[k]:        # num이 정렬 되어 있으므로, t 값이 작으면 왼쪽 포인터 증가
             i += 1
         else:
-            j -= 1
+            j -= 1              # 수가 크면 오른쪽 인덱스 감소
         
 print(cnt)
