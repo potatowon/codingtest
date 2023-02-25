c2700e581ce8c093b7edef9e8b4b11b5a2e8f54e
potatowon <yewkim0203@gmail.com>
2023-02-25 20:38:25 +0900

    modified:   .DS_Store 	modified:   "05_\354\236\220\353\243\214\352\265\254\354\241\260/06_\354\235\221\352\270\211\354\213\244.py"

--- 05_자료구조/06_응급실.py
+++ 05_자료구조/06_응급실.py
@@ -12,14 +12,30 @@ m 번째 환자가 진료받는지 출력
 
 from collections import deque
 import sys
-# n, m = map(int, sys.stdin.readline().split())
-# numbers = deque(map(int, sys.stdin.readline().split()))
+n, m = map(int, sys.stdin.readline().split())
+numbers = deque(map(int, sys.stdin.readline().split()))
 
-n, m = 6, 0
-numbers = deque([60, 60, 90, 60, 60])
-stack = []
-rt = 0
-tmp = numbers.popleft()
-if tmp < max(numbers):
-    numbers.append(tmp)
-    stack.append(tmp)
+# n, m = 5, 2
+# numbers = [60, 50, 70, 80, 90]
+q = deque()
+for idx, num in enumerate(numbers):
+    q.append((num, idx))
+cnt = 1 # 진료 순서
+
+while True:
+    tmp = q.popleft()       # 차트의 가장 앞의 환자
+    if tmp[0] < max(q)[0]:     # 위험도가 더 높은 환자가 있다면 맨 뒤로 이동
+        q.append(tmp)
+        # cnt += 1
+    else:
+        if tmp[1] == m:     # m 번째 인경우 멈추고 cnt 반환
+            break
+        else:
+            cnt += 1        # 위험도가 높은 사람이 없고, m 번째도 아니면 계속 진료
+print(cnt)
+
+
+
+'''
+max 함수의 특징 만약 list의 요소들이 튜플등으로 들어가있다면 가장 맨 앞의 원소를 기준으로 찾는다.
+'''
