02c694420a581782726cc7ea66276e9bab260b67
potatowon <yewkim0203@gmail.com>
2023-02-25 20:15:31 +0900

    0225 자료구조 코드리뷰

--- 05_자료구조/05_공주구하기.py
+++ 05_자료구조/05_공주구하기.py
@@ -4,14 +4,15 @@ N 명의 왕자 -> 나이순
 K 외치면 제외 원밖으로 -> 구구단
 '''
 from collections import deque
-n, k = int(input().split())
+n, k = map(int, input().split())
 # n, k = 8, 3
-num = deque([i for i in range(1, n+1)])
-while num:
-    num.rotate(-(k-1))
-    answer = num.popleft()
+num = deque(range(1, n+1))
+while len(num) > 1:                 # num 값이 1명 남을 때까지 반복 ( 아래 과정을 )
+    for _ in range(k-1):            # 맨 앞을 k-1번 pop 하여 뒤에 위치 -> k 번째가 맨 앞에 위치한다.
+        num.append(num.popleft())
+    num.popleft()                   # 맨 앞의 요소를 Pop 한다.
 
-print(answer)
+print(num[0])
 
 
 # while num:
--- 05_자료구조/후위식의 연산.py
+++ 05_자료구조/후위식의 연산.py
@@ -1,2 +1,28 @@
-s = input()
+'''
+후위 표기식에서 원래 식으로 만들어 계산해 내는 방식
 
+'''
+
+import sys
+
+expression = sys.stdin.readline().strip() # 후위 연산식 입력받기
+stack = []
+
+# 후위 연산식을 순회하며, 스택에 피연산자를 저장하고 연산자를 처리한다.
+for ch in expression:
+    if ch.isdigit():
+        stack.append(int(ch))
+
+    else:
+        a = stack.pop()
+        b = stack.pop()
+        if ch == '+':
+            stack.append(a + b)
+        elif ch == '-':
+            stack.append(a - b)
+        elif ch == '*':
+            stack.append(a * b)
+        elif ch == '/':
+            stack.append(a / b)
+
+print(stack[0])
