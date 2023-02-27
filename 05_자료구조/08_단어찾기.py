24e58bde6a25b6b4b10f626f555c5cfdd6104946
potatowon <yewkim0203@gmail.com>
2023-02-27 12:58:32 +0900

    0227 단어찾기 코드리뷰 (해시)

--- 05_자료구조/08_단어찾기.py
+++ 05_자료구조/08_단어찾기.py
@@ -15,8 +15,21 @@ n = int(sys.stdin.readline())
 word = [sys.stdin.readline().strip() for _ in range(n)]
 use_word = [sys.stdin.readline().strip() for _ in range(n-1)]
 
-for i in use_word:
-    if i in word:
-        word.remove(i)
+# for i in use_word:
+#     if i in word:
+#         word.remove(i) # remove 는 시간복잡도가 N 이므로 시간 초과 가능성
+#                         # Hash 테이블 이용하기
 
-print(*word)
+check_dic = {}
+
+for i in word:
+    check_dic[i] = 1 # 정해진 word에 대해 1 값
+for i in use_word:
+    check_dic[i] = 0 # 사용한 단어는 0으로 초기화 -> 사용하지 않은건 1로 남아있음
+
+for k,v in check_dic.items():
+    if v == 1:
+        print(k)
+        break
+
+# print(*word)
