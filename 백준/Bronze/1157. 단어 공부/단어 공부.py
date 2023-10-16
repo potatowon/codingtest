bae8055ac575b8e2754f8c8299d50238e0cc989e
Yewon <118495946+potatowon@users.noreply.github.com>
2023-10-16 21:18:18 +0900

    [Bronze I] Title: 단어 공부, Time: 232 ms, Memory: 33212 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -0,0 +1,19 @@
+s = input().strip()
+
+s = s.lower()
+
+d = {}
+
+for i in s:
+    if d.get(i) == None:
+        d[i] = 1
+    else:
+        d[i] += 1
+
+max_value = max(d.values())
+max_keys = [k for k, v in d.items() if v == max_value]
+
+if len(max_keys) >= 2:
+    print("?")
+else:
+    print(max_keys[0].upper())
